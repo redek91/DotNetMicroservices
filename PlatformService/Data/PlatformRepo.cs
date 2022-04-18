a184579cab42ad547ec2055d243513c853fd0b36
Manuel Roat <roat.manuel@gmail.com>
2022-04-18 09:56:32 +0200

    Initial commit

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+public class PlatformRepo : IPlatformRepo
+{
+  private readonly AppDbContext _context;
+
+  public PlatformRepo(AppDbContext context)
+  {
+    _context = context;
+  }
+  public void CreatePlatform(Platform platform)
+  {
+    _ = platform ?? throw new ArgumentNullException(nameof(platform));
+    _context.Add<Platform>(platform);
+  }
+
+  public IEnumerable<Platform> GetAllPlatforms()
+  {
+    return _context.Platforms.ToList();
+  }
+
+  public Platform GetPlatformById(int id)
+  {
+    return _context.Platforms.FirstOrDefault(x => x.Id == id);
+  }
+
+  public bool SaveChanges()
+  {
+    return _context.SaveChanges() >= 0;
+  }
+}
---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data;
+
+public static class PrepDb
+{
+  public static void PrepPopulation(IApplicationBuilder app)
+  {
+    using (var serviceScope = app.ApplicationServices.CreateScope())
+    {
+      SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+    }
+  }
+
+  private static void SeedData(AppDbContext context)
+  {
+    if (!context.Platforms.Any())
+    {
+      Console.WriteLine("--> Seeding Data...");
+      context.Platforms.AddRange(
+        new Models.Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+        new Models.Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+        new Models.Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Fundation", Cost = "Free" }
+      );
+
+      context.SaveChanges();
+    }
+    else
+    {
+      Console.WriteLine("--> We already have data");
+    }
+  }
+}
