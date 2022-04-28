36fce8b3d89c8259305009c18b6063348997617f
Manuel Roat <roat.manuel@gmail.com>
2022-04-28 18:32:52 +0200

    Added deploy script

---
+++ scripts/deploySolution.ps1
@@ -0,0 +1,7 @@
+kubectl apply -f "K8S/nginx-depl.yaml"
+kubectl apply -f "K8S/local-pvc.yaml"
+kubectl apply -f "K8S/mssql-depl.yaml"
+kubectl apply -f "K8S/rabbitmq-depl.yaml"
+kubectl apply -f "K8S/commands-depl.yaml"
+kubectl apply -f "K8S/platform-depl.yaml"
+kubectl apply -f "K8S/ingress-srv.yaml"
