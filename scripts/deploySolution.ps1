038aa4a530a2808e3b7ff076111b67d233f47e76
Manuel Roat <roat.manuel@gmail.com>
2022-04-29 19:30:49 +0200

    Use namespace

--- scripts/deploySolution.ps1
+++ scripts/deploySolution.ps1
@@ -1,10 +1,11 @@
 # This line should not be in a prod deployment for obvious security reasons
-kubectl create secret generic mssql --from-literal=SA_PASSWORD="meisterKey1$"
+kubectl create namespace dotnetmicroservice
+kubectl --namespace dotnetmicroservice create secret generic mssql --from-literal=SA_PASSWORD="meisterKey1$"
 
 kubectl apply -f "K8S/nginx-depl.yaml"
-kubectl apply -f "K8S/local-pvc.yaml"
-kubectl apply -f "K8S/mssql-depl.yaml"
-kubectl apply -f "K8S/rabbitmq-depl.yaml"
-kubectl apply -f "K8S/commands-depl.yaml"
-kubectl apply -f "K8S/platform-depl.yaml"
-kubectl apply -f "K8S/ingress-srv.yaml"
+kubectl --namespace dotnetmicroservice apply -f "K8S/local-pvc.yaml"
+kubectl --namespace dotnetmicroservice apply -f "K8S/mssql-plat-depl.yaml"
+kubectl --namespace dotnetmicroservice apply -f "K8S/rabbitmq-depl.yaml"
+kubectl --namespace dotnetmicroservice apply -f "K8S/commands-depl.yaml"
+kubectl --namespace dotnetmicroservice apply -f "K8S/platforms-depl.yaml"
+kubectl --namespace dotnetmicroservice apply -f "K8S/ingress-srv.yaml"
--- scripts/undeploySolution.ps1
+++ scripts/undeploySolution.ps1
@@ -1,20 +1,3 @@
 
-kubectl delete service commands-clusterip-srv
-kubectl delete service mssql-clusterip-srv
-kubectl delete service mssql-loadbalancer
-kubectl delete service platforms-clusterip-srv
-kubectl delete service rabbitmq-clusterip-srv
-kubectl delete service rabbitmq-loadbalancer
-
-kubectl delete deployment commands-depl
-kubectl delete deployment platforms-depl
-kubectl delete deployment rabbitmq-depl
-kubectl delete deployment mssql-depl
-
-kubectl delete ingress ingress-srv
-
+kubectl delete namespace dotnetmicroservice
 kubectl delete namespace ingress-nginx
-
-kubectl delete pvc mssql-claim
-
-kubectl delete secret mssql
